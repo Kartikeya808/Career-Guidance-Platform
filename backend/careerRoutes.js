70356772d0c01e406bff652fbfe19837d6ed5055
Kartikeya808 <kartikeyasingh8jul2005@gmail.com>
2025-11-04 19:32:19 +0530

    Delete backend/careerRoutes.js

--- backend/careerRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllCareers, addCareer } from "../controllers/careerController.js";
-const router = express.Router();
-
-router.get("/", getAllCareers);
-router.post("/", addCareer);
-
-export default router;
